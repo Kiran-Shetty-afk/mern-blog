6d18a0f0b7543cce0146c98b3edc73df740c4050
Kiran Shetty <kiranshetty2004@gmail.com>
2025-07-12 03:42:06 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,8 +7,7 @@ export default function Header() {
   const path = useLocation().pathname;
   return (
     <Navbar className='border-b-2'>
-      <Link to="/" className='self-center whitespace-nowrap text-sm 
-      sm:text-xl font-semibold dark:text-white'>
+      <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
          via-purple-500 to-pink-500 rounded-lg text-white'>Beena's</span>
         Blog
@@ -29,27 +28,19 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>
-            Sign In
-          </Button>
+          <Button gradientDuoTone='purpleToBlue' outline>Sign In</Button>
         </Link>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
         <Navbar.Link active={path === "/"} as={'div'}>
-          <Link to='/'>
-            Home
-          </Link>
+          <Link to='/'>Home</Link>
         </Navbar.Link >
         <Navbar.Link active={path === "/about"} as={'div'}>
-          <Link to='/about'>
-            About
-          </Link>
+          <Link to='/about'>About</Link>
         </Navbar.Link>
         <Navbar.Link active={path === "/projects"} as={'div'}>
-          <Link to='/projects'>
-            Projects
-          </Link>
+          <Link to='/projects'>Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
